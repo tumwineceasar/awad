8a52524a4e05c03d7dab5f47489079ec85702228
tumwineceasar <tumwineceasar@gmail.com>
2026-09-22 13:53:52 +0300

    add laravel project files

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
+
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
